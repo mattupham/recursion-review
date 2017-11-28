43d058b44dc3fe99701bc3a98d4b189f78f14341
mattupham <mcupham@gmail.com>
2017-11-27 16:01:25 -0800

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,49 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // input: any javascript value
+  // output: a JSON string
+
+  // Base cases
+  // Number
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  // Boolean true or false
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  // String - empty or not
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // null (is an object so watch out)
+  if (obj === null) {
+    return 'null';
+  }
+
+  // Recursive cases
+  // Array - empty or not - can contain other objects
+  if (Array.isArray(obj)) {
+
+    let stringifiedValues = obj.reduce(function(acc, elem) {
+      return acc += stringifyJSON(elem) + ',';
+    }, '');
+
+    return '[' + stringifiedValues.slice(0, stringifiedValues.length - 1) + ']';
+  } else {
+    // Object - empty or not - can contain other objects
+    // Termination cases
+    // Functions
+    // undefined
+    let stringifiedValues = '';
+
+    for (var key in obj) {
+      if (key !== 'functions' && key !== 'undefined') {
+        stringifiedValues += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+
+    return '{' + stringifiedValues.slice(0, stringifiedValues.length - 1) + '}';
+  }
 };
